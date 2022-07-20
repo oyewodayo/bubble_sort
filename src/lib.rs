788d247c8d769ad455c04f7e4d1cff0e21dcd4cd
Oyewo Temidayo <oyewodayo@gmail.com>
2022-07-20 21:23:05 +0100

    Bubble sort algorithm with Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,17 @@
-pub fn bubble_sort<T: PartialOrd>(v: &mut[T]){
-    for _ in 0..v.len(){
-        for i in 0..v.len() - 1{
-            if v[i] > v[i+1]{
-                v.swap(i,i+1)
+use std::fmt::Debug;
+
+pub fn bubble_sort<T: PartialOrd + Debug>(v: &mut[T]){
+    for i in 0..v.len(){       
+        let mut sorted = true;
+        for j in 0..(v.len() - 1) - i{
+            if v[j] > v[j+1]{
+                v.swap(j,j+1);
+                sorted = false
             }
+        }
+        println!("{:?}", v);
+        if sorted{
+            return;
         }
     }
 }
@@ -14,8 +22,8 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        let mut v = vec![2,4,1,5,9,3];
+        let mut v = vec![2,4,1,5,9,3,15];
         bubble_sort(&mut v);
-        assert_eq!(v, vec![1,2,3,4,9]);
+        assert_eq!(v, vec![1,2,3,4,5,9,15]);
     }
 }
